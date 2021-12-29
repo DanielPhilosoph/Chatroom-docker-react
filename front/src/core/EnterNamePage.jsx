12b696c6ce13de30fc1fa66d35e175ad092bc6e8
DanielPhilosoph <danielph9@gmail.com>
2021-12-29 12:38:05 +0200

    basic structure

---
+++ front/src/core/EnterNamePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function EnterNamePage() {
+  return (
+    <div className="EnterNamePageDiv">
+      <h1>Enter name:</h1>
+      <p>
+        <input placeholder="name" className="enterNameInput" />
+      </p>
+    </div>
+  );
+}
---
+++ front/src/core/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NavBar() {
+  return <div className="navbar" />;
+}
+
+export default NavBar;
